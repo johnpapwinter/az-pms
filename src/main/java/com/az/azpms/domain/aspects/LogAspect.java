39004557a737d53c412ae07b1c182eff8ea28601
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2024-01-14 08:50:49 +0200

    log username as well

--- src/main/java/com/az/azpms/domain/aspects/LogAspect.java
+++ src/main/java/com/az/azpms/domain/aspects/LogAspect.java
@@ -1,5 +1,7 @@
 package com.az.azpms.domain.aspects;
 
+import com.az.azpms.domain.dto.LoginDTO;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
@@ -17,7 +19,22 @@ public class LogAspect {
     }
 
     @AfterReturning(pointcut = "execution(* com.az.azpms.controller.AuthController.login(..))", returning = "result")
-    public void logLoginAttempts(ResponseEntity<?> result) {
+    public void logLoginAttempts(JoinPoint joinPoint, ResponseEntity<?> result) {
+        String username = null;
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg instanceof LoginDTO) {
+                username = ((LoginDTO) arg).getUsername();
+                break;
+            }
+        }
+
+        if (username != null) {
+            logger.info("Login attempt by {}", username);
+        } else {
+            logger.info("Login attempt by NULL username");
+        }
+
         if (result != null && result.getStatusCode().is2xxSuccessful()) {
             logger.info("Successful Login Attempt");
         } else {
